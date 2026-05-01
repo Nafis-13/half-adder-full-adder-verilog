685b75d55e15e1cc12dbd11cb2de49794d489723
TI Nafis <122612790+Nafis-13@users.noreply.github.com>
2026-05-01 22:41:44 +0600

    Create full_adder.v
    Added full adder using half adder modules

---
+++ full_adder.v
@@ -0,0 +1,29 @@
+module full_adder(
+    input a,
+    input b,
+    input cin,
+    output sum,
+    output cout
+);
+
+wire sum1;
+wire carry1;
+wire carry2;
+
+half_adder HA1 (
+    .a(a),
+    .b(b),
+    .sum(sum1),
+    .carry(carry1)
+);
+
+half_adder HA2 (
+    .a(sum1),
+    .b(cin),
+    .sum(sum),
+    .carry(carry2)
+);
+
+assign cout = carry1 | carry2;
+
+endmodule
